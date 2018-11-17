611b2dd6fc2e57c3ab03db7a5b9208a4675614b7
Kevin Ohare <oharek8@students.rowan.edu>
2018-11-17 14:59:30 -0500

    Update

--- Code/main.c
+++ Code/main.c
@@ -6,6 +6,10 @@
  */
 
 unsigned int setTemp = 20;                          // Initial Set Temp (20 Celcius)
+unsigned int currentTemp = 0;                       // Current Temp
+float tempTemp;
+
+unsigned int count = 0;
 
 int main(void)
 {
@@ -42,15 +46,16 @@ int main(void)
     UCA1IFG &= ~UCRXIFG;                        // Clear Interrupt Flag
 
     ADC12CTL0 = ADC12SHT02 + ADC12ON;           // Sampling time, ADC12 on
-      ADC12CTL1 = ADC12SHP;                     // Use sampling timer
-      ADC12IE = 0x01;                           // Enable interrupt
-      ADC12CTL0 |= ADC12ENC;
-      P6SEL |= 0x01;                            // P6.0 ADC option select
-      P1DIR |= 0x01;                            // P1.0 output
+    ADC12CTL1 = ADC12SHP;                       // Use sampling timer
+    ADC12IE = 0x01;                             // Enable interrupt
+    ADC12CTL0 |= ADC12ENC;
+    P6SEL |= 0x01;                              // P6.0 ADC option select
+    P1DIR |= 0x01;                              // P1.0 output
 
       while (1)
       {
         ADC12CTL0 |= ADC12SC;                   // Start sampling/conversion
+
 
         __bis_SR_register(LPM0_bits + GIE);     // LPM0, ADC12_ISR will force exit
         __no_operation();                       // For debugger
@@ -67,12 +72,25 @@ __interrupt void ADC12_ISR(void)
   case  2: break;                           // Vector  2:  ADC overflow
   case  4: break;                           // Vector  4:  ADC timing overflow
   case  6:                                  // Vector  6:  ADC12IFG0
-    if (ADC12MEM0 >= 0x7ff)                 // ADC12MEM = A0 > 0.5AVcc?
-      P1OUT |= BIT0;                        // P1.0 = 1
-    else
-      P1OUT &= ~BIT0;                       // P1.0 = 0
 
-    //setTemp = ADC12MEM0;                  // This ain't what it do
+    //float sampleADC = ADC12MEM0;
+    tempTemp = (ADC12MEM0 * (3.3 / 4095) - 0.424) / 0.00625;
+
+    currentTemp = (int)tempTemp;
+
+    if(count >= 1)
+    {
+        break;
+    }
+    else
+    {
+        while(!(UCA1IFG & UCTXIFG));
+        UCA1TXBUF = currentTemp + '0';
+
+        count++;
+    }
+
+
 
     __bic_SR_register_on_exit(LPM0_bits);   // Exit active CPU
   case  8: break;                           // Vector  8:  ADC12IFG1
@@ -98,37 +116,5 @@ __interrupt void ADC12_ISR(void)
 __interrupt void USCI_A1_ISR(void)
 {
     setTemp = UCA1RXBUF;                        // Might change depending on value sent through uart
-
-  /*switch(byte)
-  {
-  case 0:
-      size = UCA1RXBUF;                         // Save Packet Size
-      break;                                    // Vector 0 - no interrupt
-  case 1:
-      TA0CCR1 = UCA1RXBUF;                      // Sets Red PWM
-      break;
-  case 2:
-      TA0CCR2 = UCA1RXBUF;                      // Sets Green PWM
-      break;
-  case 3:
-      TA0CCR3 = UCA1RXBUF;                      // Sets Blue PWM
-      while(!(UCA1IFG & UCTXIFG));
-          UCA1TXBUF = size - 3;
-      break;
-  default:
-      if(byte > size)
-      {
-          byte = -1;
-          size = 0;
-      }
-      else
-      {
-          while(!(UCA1IFG & UCTXIFG));
-              UCA1TXBUF = UCA1RXBUF;
-      }
-      break;
-  }
-  byte++;
-  */
 }
 
